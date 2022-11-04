055f064270d8916b7299cf20558dad1530b839a8
Afonso <afonsobezerra199@gmail.com>
2022-11-04 04:12:01 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useMemo } from "react";
+import "./App.css";
+
+export default function App() {
+  const [task, setTask] = React.useState<any>([]);
+  
+  useEffect(() => {
+    const taskLocal:any = localStorage.getItem("task")
+    const taskJson= JSON.parse(taskLocal)
+    setTask(taskJson)
+  }, []);
+
+
+  function createNewTask(e:any) {
+    e.preventDefault()
+    const data = {
+      id: Math.floor(Date.now() * Math.random()).toString(36),
+      name: e.target.taskName.value,
+      complete: false
+    };
+    setTask([data, ...task]);
+    e.target.taskName.value = "";
+  }
+
+  function deleteTask(id:any) {
+    const index = task.findIndex((e:any) => e.id === id);
+    setTask(task.filter((x:any, i:any) => i !== index));
+    setLocalStorage();
+  }
+  function complete(id:any, value:any) {
+    setTask(task.map((task:any) =>{
+      if(task.id === id){
+        return {...task, complete: value}
+      }
+      else{
+        return task
+      }
+    }))
+  }
+
+  useMemo(() => {task.length ? setLocalStorage() : null }, [task])
+  
+  function setLocalStorage(){
+    localStorage.setItem("task", JSON.stringify(task));
+  }
+  
+  return (
+    <div className="App">
+      <h1>Hello </h1>
+      <form onSubmit={(e) => createNewTask(e)}>
+        <input type="text" required placeholder="Create new task" name="taskName" />
+        <button type="submit">Add Task</button>
+      </form>
+      <div className="taskContainer">
+        {task.map((element:any) => (
+          <div
+            className="tasks"
+            key={element.key}
+            style={
+              element.complete
+                ? { background: "green" }
+                : { background: "gray" }
+            }
+          >
+             <div className="name">
+              <input
+                type="checkbox"
+                onChange={(e) => complete(element.id, e.target.checked)}
+                checked={element.complete}
+              />
+              {element.name}
+            </div>
+            <button onClick={() => deleteTask(element.id)}>Delete</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
